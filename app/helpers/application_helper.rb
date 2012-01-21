436cc6cfe8563cfad3d889a6498220986fbbf2fb
Ashton Harris <ahdesignsonline@gmail.com>
2012-01-21 09:19:58 -0500

    nothing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def link_to_with_current_class(name, options)
+      if current_page?(options)
+      content_tag :li, link_to(h(name), options), :class => "current"
+    else
+      content_tag :li, link_to(h(name), options)
+    end   
+  end
+  
 end
