0c81c655179dc1f4a64ed562131d2495dfc98aac
Ashton Harris <ahdesignsonline@gmail.com>
2012-01-21 19:02:08 -0500

    added classes to nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   
   def link_to_with_current_class(name, options)
       if current_page?(options)
-      content_tag :li, link_to(h(name), options), :class => "current"
+      content_tag :li, link_to(h(name), options, :class => name), :class => "current"
     else
-      content_tag :li, link_to(h(name), options)
+      content_tag :li, link_to(h(name), options, :class => name)
     end   
   end
   
