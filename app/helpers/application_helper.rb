adc5459aa596080385a4aded9e54725bf2143994
Ashton Harris <ahdesignsonline@gmail.com>
2012-02-04 17:12:00 -0500

    added all_work page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def link_to_with_current_class(name, path)
-    if path == request.path or (path == "/" and request.path.include? "/work/")
+    if path == request.path or (path == "/all_work" and request.path.include? "/work/")
       content_tag :li, link_to(name, path, :class => name), :class => "current"
     else
       content_tag :li, link_to(name, path, :class => name)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Ashtonharris::Application.routes.draw do
   get 'about' => 'pages#about' 
   get  "contact", :to => "contact#new", :as => "contact"
   post "contact", :to => "contact#create"
+  get 'all_work', :to => 'pages#all_work'
    
   # work example link links
   get '/work/:page', :to => 'work#show',  :as => "work"
